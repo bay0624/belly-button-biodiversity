6aeba95982518a06a11859dc9c947f95dc2d6dca
Abayomi Olujobi <bay.olu87@gmail.com>
2021-07-13 21:55:03 -0400

    Move sample ids to dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,13 +6,18 @@ Feel free to disregard and create your own code */
 function init() {
 
     // Read json data
+    d3.json("samples.json").then((data) => {
+        console.log(data)
+        // Parsing and filtering data to get sample ids
+        // Moving sample ids to dropdown menu
+        let dropdown = d3.select("#selDataset")
+        data.names.forEach(function(id) { 
+            dropdown.append("option").text(id).property("value");
+        });
 
-        // Parse and filter data to get sample names
+        // Call functions below using the first sample to build metadata and initial plots
 
-        // Add dropdown option for each sample
-
-    // Call functions below using the first sample to build metadata and initial plots
-
+    });
 }
 
 // Define a function that will create metadata for given sample
@@ -20,9 +25,9 @@ function buildMetadata(sample) {
 
     // Read the json data
 
-        // Parse and filter the data to get the sample's metadata
+    // Parse and filter the data to get the sample's metadata
 
-        // Specify the location of the metadata and update it
+    // Specify the location of the metadata and update it
 
 }
 
@@ -31,17 +36,17 @@ function buildCharts(sample) {
 
     // Read the json data
 
-        // Parse and filter the data to get the sample's OTU data
-        // Pay attention to what data is required for each chart
+    // Parse and filter the data to get the sample's OTU data
+    // Pay attention to what data is required for each chart
 
-        // Create bar chart in correct location
+    // Create bar chart in correct location
 
-        // Create bubble chart in correct location
-    
+    // Create bubble chart in correct location
+
 }
 
 
-function optionChanged(sample){
+function optionChanged(sample) {
     // The parameter being passed in this function is new sample id from dropdown menu
 
     // Update metadata with newly selected sample
